4a39cde6b495450f35835f33726ef368fc2276b2
Randy Moreau <115094208+randysnowflake74@users.noreply.github.com>
2022-10-05 13:46:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-Breakfast Menu
+streamlit.header('Breakfast Menu')
 
-Omega 3 & Blueberry Oatmeal
+streamlit.text('Omega 3 & Blueberry Oatmeal')
